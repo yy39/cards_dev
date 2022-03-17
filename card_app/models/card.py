7a8089f37c3fd24bcf58cfc5f21ed6c0ac06d1fa
Yusan Yin <yusanyin@gmail.com>
2022-03-17 15:23:17 -0700

    add button styling

--- card_app/models/card.py
+++ card_app/models/card.py
@@ -1,11 +1,12 @@
 from card_app.config.mysqlconnection import connectToMySQL, queryMySQL
 
+
 class Card:
     def __init__(self, data):
         self.name = data['name']
         self.type = data['type']
         self.value = data['value']
-    
+
     @classmethod
     def create_card(cls, data):
         query = 'INSERT INTO cards (type, name, value) VALUES (%(type)s, %(name)s, %(value)s);'
