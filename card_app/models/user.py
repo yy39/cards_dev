f7309a04b9a06541af2e277ed55a06e731321c2d
Yusan Yin <yusanyin@gmail.com>
2022-03-18 13:06:46 -0700

    add more email validation and initial unique validation

--- card_app/models/user.py
+++ card_app/models/user.py
@@ -36,16 +36,31 @@ class User:
     @staticmethod
     def validate(user):
         is_valid = True
+        # username validation
         if len(user['username']) < 3:
             flash('Name must be at least 3 characters.')
             is_valid = False
+        # display_name validat8on
         if len(user['display_name']) < 3:
             flash('Display name must be at least 3 characters.')
             is_valid = False
+        # email validation
         if len(user['email']) < 5:
             flash('Email must be at least 5 characters.')
             is_valid = False
+        elif len(user['email']) >= 255:
+            flash('Email must be less than 255 characters.')
+            is_valid = False
+        elif not EMAIL_REGEX.match(user['email']):
+            flash('Invalid email address')
+            is_valid = False
+        # password validation
         if len(user['password']) < 8:
             flash('Password must be at least 8 characters')
             is_valid = False
+        # unique validation
+        if is_valid:
+            query = "SELECT username, email FROM users;"
+            uniques = connectToMySQL('card_app_db_test').query_db(query)
+            print(uniques)
         return is_valid
